266b3eeb007e6f7118a30490a123f05ae67202ff
Guillermo Brachetta <brachetta@me.com>
2020-09-22 12:28:29 +0200

    Basic display of CDs in shop page

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Cd
 
 
 # Create your views here.
 def shop(request):
-    return render(request, 'shop/shop.html')
+    cds = Cd.objects.all
+    context = {
+        'cds': cds,
+    }
+    return render(request, 'shop/shop.html', context)
