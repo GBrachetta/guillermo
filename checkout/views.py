5c634a95f11987bd98bbd07a7208f810e612627a
Guillermo Brachetta <brachetta@me.com>
2020-09-30 10:09:25 +0200

    Added stripe elements to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
+        "stripe_public_key": "pk_test_51FFigNKZb5qTUkA0O38c6pEm9sgJ4YVEvJU05ZblVHcFy7SB7dMnEBQBm0sChReJxPUrNJXJO7evtfVoV8QUy6k900B35D0E3t",
+        "client_secret": "My deep secret!",
     }
 
     return render(request, template, context)
