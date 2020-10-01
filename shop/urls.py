69d0ed16ba2c1f13162792e3e2a7ea073b78a371
Guillermo Brachetta <brachetta@me.com>
2020-10-02 00:16:05 +0200

    Add cds view, url and template

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.shop, name='shop'),
     path("<int:cd_id>/", views.cd_detail, name="cd_detail"),
+    path("add/", views.add_cd, name="add_cd"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Cd
+from .forms import CdForm
 
 
 # Create your views here.
 def shop(request):
     cds = Cd.objects.all
     context = {
-        'cds': cds,
+        "cds": cds,
     }
-    return render(request, 'shop/shop.html', context)
+    return render(request, "shop/shop.html", context)
 
 
 def cd_detail(request, cd_id):
@@ -21,3 +22,11 @@ def cd_detail(request, cd_id):
     }
 
     return render(request, "shop/cd_details.html", context)
+
+
+def add_cd(request):
+    form = CdForm
+    template = "shop/add_cd.html"
+    context = {"form": form}
+
+    return render(request, template, context)
