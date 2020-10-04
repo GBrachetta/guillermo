45760ee065e5d1838fb90bdf027e142930f9a57e
Guillermo Brachetta <brachetta@me.com>
2020-10-04 11:13:30 +0200

    Reformatted masonry grid for more responsiveness and even layout on different sizes

--- album/static/album/js/album.js
+++ album/static/album/js/album.js
@@ -1,17 +1,32 @@
-var elem = document.querySelector(".grid");
-var msnry = new Masonry(elem, {
-    // options
+// var elem = document.querySelector(".grid");
+// var msnry = new Masonry(elem, {
+//     // options
+//     itemSelector: ".grid-item",
+//     columnWidth: 200,
+// });
+
+// // element argument can be a selector string
+// //   for an individual element
+// var msnry = new Masonry(".grid", {
+//     // options
+// });
+
+// lightbox.option({
+//     wrapAround: true,
+//     showImageNumberLabel: false,
+// });
+
+
+
+var grid = document.querySelector(".grid");
+
+var msnry = new Masonry(grid, {
     itemSelector: ".grid-item",
-    columnWidth: 200,
+    columnWidth: ".grid-sizer",
+    percentPosition: true,
 });
 
-// element argument can be a selector string
-//   for an individual element
-var msnry = new Masonry(".grid", {
-    // options
-});
-
-lightbox.option({
-    wrapAround: true,
-    showImageNumberLabel: false,
+imagesLoaded(grid).on("progress", function () {
+    // layout Masonry after each image loads
+    msnry.layout();
 });
