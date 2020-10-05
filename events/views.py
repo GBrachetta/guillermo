361bbfafc0cbe80731db5e7ea7ffd8c143b85ef3
Guillermo Brachetta <brachetta@me.com>
2020-10-06 01:00:43 +0200

    Further styled table of events past and future

--- events/views.py
+++ events/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Event
 
 
 # Create your views here.
 def events(request):
-    context = {"events": "active"}
+    events = Event.objects.all().order_by("date")
+    context = {"calendar": events, "events": "active"}
     return render(request, "events/events.html", context)
