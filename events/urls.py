e0cdb908a117f3c04be1767b846d14d108c73a2b
Guillermo Brachetta <brachetta@me.com>
2020-10-06 14:49:53 +0200

    Created add_event, edit_event and delete_event views, forms and templates

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.events, name="events"),
+    path("add_event/", views.add_event, name="add_event"),
+    path("edit_event/<int:event_id>/", views.edit_event, name="edit_event"),
+    path(
+        "delete_event/<int:event_id>/", views.delete_event, name="delete_event"
+    ),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Event
+from .forms import EventForm
 
 
 # Create your views here.
@@ -7,3 +10,56 @@ def events(request):
     events = Event.objects.all().order_by("date")
     context = {"calendar": events, "events": "active"}
     return render(request, "events/events.html", context)
+
+
+@login_required
+def add_event(request):
+    if not request.user.is_superuser:
+        messages.error(request, "Only administrators can access this url.")
+        return redirect(reverse("events"))
+
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Event added.")
+            return redirect(reverse("events"))
+        else:
+            messages.error(
+                request, "Failed to add event. Please check your form."
+            )
+    else:
+        form = EventForm()
+    template = "events/add_event.html"
+    context = {"form": form}
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_event(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+
+    if request.method == "POST":
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your event has been updated.")
+            return redirect("events")
+        else:
+            messages.error(request, "Please check the information submitted.")
+    else:
+        form = EventForm(instance=event)
+        messages.info(request, f"You are editing {event.name}")
+    template = "events/edit_event.html"
+    context = {"form": form, "event": event}
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_event(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    event.delete()
+    messages.success(request, "Event deleted.")
+    return redirect(reverse("events"))
