d1e764df99b2d84941be97b6c7ff79309266d02a
Guillermo Brachetta <brachetta@me.com>
2020-10-08 00:16:20 +0200

    Reduced font text in event grid for small devices

--- static/js/extras.js
+++ static/js/extras.js
@@ -2,10 +2,10 @@
 $(".toast").toast("show");
 
 // Smooth opening navbar dropdown
-$(".dropdown").on("show.bs.dropdown", function (e) {
+$(".dropdown").on("show.bs.dropdown", function () {
     $(this).find(".dropdown-menu").first().stop(true, true).slideDown(300);
 });
 
-$(".dropdown").on("hide.bs.dropdown", function (e) {
+$(".dropdown").on("hide.bs.dropdown", function () {
     $(this).find(".dropdown-menu").first().stop(true, true).slideUp(300);
 });
