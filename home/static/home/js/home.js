a7128f77ea1ec700d0674b177a6bf9754aebfa85
Guillermo Brachetta <brachetta@me.com>
2020-10-08 17:25:21 +0200

    Attempting new transitions for landing page

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -1,48 +1,46 @@
-$(document).ready(function () {
-    // Init ScrollMagic
-    const controller = new ScrollMagic.Controller();
-
-    // Scene intro - pin the intro section
-
-    const tween = TweenMax.to(".background-container", 1, {
-        scale: 2,
-        opacity: 0,
-        // ease: Linear.easeNone,
-        ease: Power2.easeOut,
-    });
-
-    // const tweenImg02 = TweenMax.to(".img-02", 1, {
-    //     scale: 2,
-    //     opacity: 0,
-    //     ease: Linear.easeNone,
-    // });
-
-    const pinSceneIntro = new ScrollMagic.Scene({
-        triggerElement: "#intro",
-        triggerHook: 0,
-        duration: "200%",
-    })
-        .setTween(tween)
-        .setPin("#intro .background-container")
-        .addIndicators()
-        .addTo(controller);
-
-    // Scene 1 - pin the second section
-    var pinScene01 = new ScrollMagic.Scene({
-        triggerElement: "#slide01",
-        triggerHook: 0,
-        duration: "100%",
-    })
-        .setPin("#slide01 .pin-wrapper")
-        .addIndicators()
-        .addTo(controller);
-    // Scene 2 - pin the third section
-    var pinScene02 = new ScrollMagic.Scene({
-        triggerElement: "#slide01",
-        triggerHook: 0,
-        duration: "200%",
-    })
-        .addIndicators()
-        .setPin("#slide02 .pin-wrapper")
-        .addTo(controller);
+gsap.to(".red", {
+    opacity: 0,
+    // zoom: 1.2,
+    scrollTrigger: {
+        trigger: ".red",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        // markers: true,
+    },
 });
+
+gsap.to(".orange", {
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".orange",
+        start: "top bottom-=600",
+        end: "bottom bottom-=1400",
+        scrub: true,
+        // markers: true,
+    },
+});
+
+gsap.to(".purple", {
+    opacity: 0.3,
+    scrollTrigger: {
+        trigger: ".purple",
+        start: "top bottom-=600",
+        end: "bottom bottom",
+        scrub: true,
+        // markers: true,
+    },
+});
+
+// gsap.utils.toArray(".panel").forEach((panel) => {
+//     ScrollTrigger.create({
+//         trigger: panel,
+//         start: "top top-=300",
+//         pin: true,
+//         pinSpacing: false,
+//     });
+// });
+
+// ScrollTrigger.create({
+//     snap: 1 / 2, // snap whole page to the closest section!
+// });
