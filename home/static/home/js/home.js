68420bf3fcdf4cd746123a4f876fa83fb59e382a
Guillermo Brachetta <brachetta@me.com>
2020-10-26 21:31:18 +0100

    Test for responsive background images and parallax on deployment

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -1,99 +1,163 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#overlay-first-section", {
-    opacity: 1,
-    scrollTrigger: {
-        trigger: "#overlay-first-section",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-    },
-});
+// gsap.to("#overlay-first-section", {
+//     opacity: 1,
+//     scrollTrigger: {
+//         trigger: "#overlay-first-section",
+//         start: "top top",
+//         end: "bottom top",
+//         scrub: true,
+//     },
+// });
 
-gsap.from("#overlay-second-section", {
-    opacity: 1,
-    scrollTrigger: {
-        trigger: "#overlay-second-section",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-    },
-});
+// gsap.from("#overlay-second-section", {
+//     opacity: 1,
+//     scrollTrigger: {
+//         trigger: "#overlay-second-section",
+//         start: "top top",
+//         end: "bottom top",
+//         scrub: true,
+//     },
+// });
 
-gsap.from("#overlay-third-section", {
-    opacity: 1,
-    scrollTrigger: {
-        trigger: "#overlay-third-section",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-    },
-});
+// gsap.from("#overlay-third-section", {
+//     opacity: 1,
+//     scrollTrigger: {
+//         trigger: "#overlay-third-section",
+//         start: "top top",
+//         end: "bottom top",
+//         scrub: true,
+//     },
+// });
 
-gsap.to("#overlay-fourth-section", {
-    opacity: 1,
-    scrollTrigger: {
-        trigger: "#overlay-third-section",
-        start: "bottom bottom+=200",
-        end: "bottom",
-        scrub: true,
-    },
-});
+// gsap.to("#overlay-fourth-section", {
+//     opacity: 1,
+//     scrollTrigger: {
+//         trigger: "#overlay-third-section",
+//         start: "bottom bottom+=200",
+//         end: "bottom",
+//         scrub: true,
+//     },
+// });
 
-gsap.to("#first-section-content", {
-    opacity: 0,
-    y: "500",
-    scrollTrigger: {
-        trigger: "#first-section",
-        start: "top top",
-        end: "+=800",
-        scrub: true,
-    },
-});
+// gsap.to("#first-section-content", {
+//     opacity: 0,
+//     y: "500",
+//     scrollTrigger: {
+//         trigger: "#first-section",
+//         start: "top top",
+//         end: "+=800",
+//         scrub: true,
+//     },
+// });
 
-gsap.to("#second-section-content", {
-    opacity: 1,
-    x: "15%",
-    scrollTrigger: {
-        trigger: "#first-section",
-        start: "center",
-        end: "+=600",
-        scrub: true,
-    },
-});
+// gsap.to("#second-section-content", {
+//     opacity: 1,
+//     x: "15%",
+//     scrollTrigger: {
+//         trigger: "#first-section",
+//         start: "center",
+//         end: "+=600",
+//         scrub: true,
+//     },
+// });
 
-gsap.to("#third-section-content", {
-    opacity: 1,
-    x: "-20%",
-    scrollTrigger: {
-        trigger: "#second-section",
-        start: "center",
-        end: "+=600",
-        scrub: true,
-    },
-});
+// gsap.to("#third-section-content", {
+//     opacity: 1,
+//     x: "-20%",
+//     scrollTrigger: {
+//         trigger: "#second-section",
+//         start: "center",
+//         end: "+=600",
+//         scrub: true,
+//     },
+// });
 
-gsap.to("#fourth-section-content", {
-    opacity: 1,
-    y: "500px",
-    scrollTrigger: {
-        trigger: "#third-section",
-        start: "center",
-        end: "+=600",
-        scrub: true,
-    },
-});
+// gsap.to("#fourth-section-content", {
+//     opacity: 1,
+//     y: "500px",
+//     scrollTrigger: {
+//         trigger: "#third-section",
+//         start: "center",
+//         end: "+=600",
+//         scrub: true,
+//     },
+// });
 
-// PARALLAX
+// // PARALLAX
+
+// gsap.utils.toArray("section").forEach((section, i) => {
+//     section.panel = section.querySelector(".panel");
+
+//     // Do the parallax effect on each section
+//     if (i) {
+//         section.panel.style.backgroundPosition = `50% ${innerHeight / 2}px`;
+
+//         gsap.to(section.panel, {
+//             backgroundPosition: `50% ${-innerHeight / 2}px`,
+//             ease: "none",
+//             scrollTrigger: {
+//                 trigger: section,
+//                 scrub: true,
+//             },
+//         });
+//     }
+
+//     // the first image should be positioned against the top. Use px on the animating part to work with GSAP.
+//     else {
+//         section.panel.style.backgroundPosition = "50% 0px";
+
+//         gsap.to(section.panel, {
+//             backgroundPosition: `50% ${-innerHeight / 2}px`,
+//             ease: "none",
+//             scrollTrigger: {
+//                 trigger: section,
+//                 start: "top top",
+//                 end: "bottom top",
+//                 scrub: true,
+//             },
+//         });
+//     }
+// });
+
+// // Snap sections
+
+// // function goToSection(i, anim) {
+// //     gsap.to(window, {
+// //         scrollTo: { y: i * innerHeight, autoKill: false },
+// //         duration: 2,
+// //     });
+
+// //     if (anim) {
+// //         anim.restart();
+// //     }
+// // }
+
+// // gsap.utils.toArray(".panel").forEach((panel, i) => {
+// //     ScrollTrigger.create({
+// //         trigger: panel,
+// //         onEnter: () => goToSection(i),
+// //     });
+
+// //     ScrollTrigger.create({
+// //         trigger: panel,
+// //         start: "bottom bottom",
+// //         onEnterBack: () => goToSection(i),
+// //     });
+// // });
+
 
 gsap.utils.toArray("section").forEach((section, i) => {
-    section.panel = section.querySelector(".panel");
+    section.bg = section.querySelector(".bg");
+
+    // Give the backgrounds some random images
+    // section.bg.style.backgroundImage = `url(https://picsum.photos/${innerWidth}/${innerHeight}?random=${i})`;
 
     // Do the parallax effect on each section
     if (i) {
-        section.panel.style.backgroundPosition = `50% ${innerHeight / 2}px`;
+        section.bg.style.backgroundPosition = `50% ${innerHeight / 2}px`;
 
-        gsap.to(section.panel, {
+        gsap.to(section.bg, {
             backgroundPosition: `50% ${-innerHeight / 2}px`,
             ease: "none",
             scrollTrigger: {
@@ -105,9 +169,9 @@ gsap.utils.toArray("section").forEach((section, i) => {
 
     // the first image should be positioned against the top. Use px on the animating part to work with GSAP.
     else {
-        section.panel.style.backgroundPosition = "50% 0px";
+        section.bg.style.backgroundPosition = "50% 0px";
 
-        gsap.to(section.panel, {
+        gsap.to(section.bg, {
             backgroundPosition: `50% ${-innerHeight / 2}px`,
             ease: "none",
             scrollTrigger: {
@@ -119,29 +183,3 @@ gsap.utils.toArray("section").forEach((section, i) => {
         });
     }
 });
-
-// Snap sections
-
-// function goToSection(i, anim) {
-//     gsap.to(window, {
-//         scrollTo: { y: i * innerHeight, autoKill: false },
-//         duration: 2,
-//     });
-
-//     if (anim) {
-//         anim.restart();
-//     }
-// }
-
-// gsap.utils.toArray(".panel").forEach((panel, i) => {
-//     ScrollTrigger.create({
-//         trigger: panel,
-//         onEnter: () => goToSection(i),
-//     });
-
-//     ScrollTrigger.create({
-//         trigger: panel,
-//         start: "bottom bottom",
-//         onEnterBack: () => goToSection(i),
-//     });
-// });
