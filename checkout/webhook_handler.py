1172b8e0d7c5fc6b2b347d3f355bd7a1b9fc2239
Guillermo Brachetta <brachetta@me.com>
2020-11-01 13:57:23 +0100

    Setting up multiple emails for customer and admin on new order

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.core.mail import send_mail
+from django.core import mail
 from django.template.loader import render_to_string
 from django.conf import settings
 from .models import Order, OrderLineItem
@@ -13,34 +14,69 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
+    # def _send_confirmation_email(self, order):
+    #     cust_email = order.email
+    #     subject = render_to_string(
+    #         "checkout/confirmation_emails/confirmation_email_subject.txt",
+    #         {"order": order},
+    #     )
+    #     body = render_to_string(
+    #         "checkout/confirmation_emails/confirmation_email_body.txt",
+    #         {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
+    #     )
+    #     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
+
     def _send_confirmation_email(self, order):
-        cust_email = order.email
-        subject = render_to_string(
+        subject_customer = render_to_string(
             "checkout/confirmation_emails/confirmation_email_subject.txt",
             {"order": order},
         )
-        body = render_to_string(
+        body_customer = render_to_string(
             "checkout/confirmation_emails/confirmation_email_body.txt",
             {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
         )
-        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
-
-    def _send_new_order_email(self, order):
-        admin_email = "brachetta@me.com"
-        subject = render_to_string(
+        subject_admin = render_to_string(
             "checkout/confirmation_emails/new_order_email_subject.txt",
             {"order": order},
         )
-        body = render_to_string(
+        body_admin = render_to_string(
             "checkout/confirmation_emails/new_order_email_body.txt",
             {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
         )
-        send_mail(
-            subject,
-            body,
+        connection = mail.get_connection()
+        cust_email = order.email
+        connection.open()
+        customer_email = mail.EmailMessage(
+            subject_customer,
+            body_customer,
             settings.DEFAULT_FROM_EMAIL,
-            [admin_email],
+            [cust_email],
         )
+        admin_email = mail.EmailMessage(
+            subject_admin,
+            body_admin,
+            settings.DEFAULT_FROM_EMAIL,
+            [settings.DEFAULT_ADMIN_EMAIL],
+        )
+        connection.send_messages([customer_email, admin_email])
+        connection.close()
+
+    # def _send_new_order_email(self, order):
+    #     admin_email = "brachetta@me.com"
+    #     subject = render_to_string(
+    #         "checkout/confirmation_emails/new_order_email_subject.txt",
+    #         {"order": order},
+    #     )
+    #     body = render_to_string(
+    #         "checkout/confirmation_emails/new_order_email_body.txt",
+    #         {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
+    #     )
+    #     send_mail(
+    #         subject,
+    #         body,
+    #         settings.DEFAULT_FROM_EMAIL,
+    #         [admin_email],
+    #     )
 
     def handle_event(self, event):
         return HttpResponse(
@@ -103,7 +139,7 @@ class StripeWH_Handler:
                 time.sleep(1)
         if order_exists:
             self._send_confirmation_email(order)
-            self._send_new_order_email(order)
+            # self._send_new_order_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order in database',
                 status=200,
@@ -140,7 +176,7 @@ class StripeWH_Handler:
                     status=500,
                 )
         self._send_confirmation_email(order)
-        self._send_new_order_email(order)
+        # self._send_new_order_email(order)
         return HttpResponse(
             content=(
                 f'Webhook received: {event["type"]} | SUCCESS: '
