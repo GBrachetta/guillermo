5ce1b59a248484f90f4e87b51e59c4e40d66e8fe
Guillermo Brachetta <brachetta@me.com>
2020-11-02 15:13:14 +0100

    Collapsed navbar closes when clicked outside

--- static/js/extras.js
+++ static/js/extras.js
@@ -9,3 +9,13 @@ $(".dropdown").on("show.bs.dropdown", function () {
 $(".dropdown").on("hide.bs.dropdown", function () {
     $(this).find(".dropdown-menu").first().stop(true, true).slideUp(300);
 });
+
+$(document).ready(function () {
+    $(document).click(function (event) {
+        const click = $(event.target);
+        const _open = $(".navbar-collapse").hasClass("show");
+        if (_open === true && !click.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+});
