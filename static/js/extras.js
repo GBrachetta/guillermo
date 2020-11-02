a409c3c388bc35a6f4b6b5a10b1df6e84e6dd25d
Guillermo Brachetta <brachetta@me.com>
2020-11-02 22:25:22 +0100

    Added function to pass form to modal

--- static/js/extras.js
+++ static/js/extras.js
@@ -10,12 +10,20 @@ $(".dropdown").on("hide.bs.dropdown", function () {
     $(this).find(".dropdown-menu").first().stop(true, true).slideUp(300);
 });
 
-$(document).ready(function () {
-    $(document).click(function (event) {
+$(function () {
+    $(document).on("click", function (event) {
         const click = $(event.target);
         const _open = $(".navbar-collapse").hasClass("show");
         if (_open === true && !click.hasClass("navbar-toggler")) {
             $(".navbar-toggler").click();
         }
+    });
+});
+
+$(function () {
+    $("#trigger-contact").on("click", () => {
+        $("#form-modal").load("/contact", () => {
+            $("#contact").modal("show");
+        });
     });
 });
