4b50b351f373cce42889a27e9dc8605e09304335
Guillermo Brachetta <brachetta@me.com>
2020-11-07 21:57:19 +0100

    Fixed parallax with function value of y

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -40,53 +40,61 @@ gsap.to(".fourth-overlay", {
 
 // Calculates height of parent container to position section content
 yTo = $(".parent-container").innerHeight() / 2;
-
-// Reloads screen on orientation change so new yTo value can be applied
-window.onorientationchange = () => {
-    window.location.reload();
-};
+const selector = $(".parent-container");
 
 gsap.to("#first-section-content", 3, {
     opacity: 0,
-    y: yTo,
+    y: () => {
+        return selector.innerHeight() / 2;
+    },
     scrollTrigger: {
         trigger: ".first-overlay",
         start: "top top",
         end: "bottom center",
         scrub: true,
+        invalidateOnRefresh: true,
     },
 });
 
 gsap.to("#second-section-content", 3, {
-    y: (yTo += 10),
+    y: () => {
+        return selector.innerHeight() / 2;
+    },
     opacity: 1,
     scrollTrigger: {
         trigger: ".first-overlay",
         start: "+=60%",
         end: "+=220",
         scrub: true,
+        invalidateOnRefresh: true,
     },
 });
 
 gsap.to("#third-section-content", {
     opacity: 1,
-    y: (yTo -= 80),
+    y: () => {
+        return selector.innerHeight() / 2 - 80;
+    },
     scrollTrigger: {
         trigger: ".second-overlay",
         start: "+=60%",
         end: "+=220",
         scrub: true,
+        invalidateOnRefresh: true,
     },
 });
 
 gsap.to("#fourth-section-content", {
     opacity: 1,
-    y: (yTo += 80),
+    y: () => {
+        return selector.innerHeight() / 2;
+    },
     scrollTrigger: {
         trigger: ".third-overlay",
         start: "+=60%",
         end: "+=200",
         scrub: true,
+        invalidateOnRefresh: true,
     },
 });
 
