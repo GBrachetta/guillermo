a0ef1867e24ab8f32b87378b874776c185479346
Guillermo Brachetta <brachetta@me.com>
2020-11-09 14:29:47 +0100

    Added docstrings to home app

--- home/views.py
+++ home/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    """ renders the landing page """
+    """
+    Renders the landing page
+    """
     context = {"home": "active"}
     return render(request, "home/home.html", context)
